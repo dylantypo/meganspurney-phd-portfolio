d5fcf3e7f5c9581514c58f19c338761b2069fa1e
dylantypo <dylantylerposner@gmail.com>
2025-06-17 23:21:12 -0500

    loading...

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>MS</h1>
+<p>...loading</p>
+<p>Visit <a href="https://meganspurney.github.io/">meganspurney.github.io</a> to see current work.</p>
