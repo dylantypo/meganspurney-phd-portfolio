59d260855a189667ff579dc67e7197ea17dae2e0
dylantypo <dylantylerposner@gmail.com>
2025-06-18 19:09:54 -0500

    importing old files and improving file structure [STABLE]

---
+++ src/routes/+error.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+</script>
+
+<main>
+	<section class="message">
+		<button onclick={() => goto('/')}>
+			<h1>Error 404: Page Not Found</h1>
+		</button>
+	</section>
+</main>
+
+<style>
+	/* @font-face {
+		font-family: 'KenneyFuture';
+		src: url('/fonts/Kenney%20Future.woff2') format('woff2');
+		font-display: swap;
+	} */
+
+	/* Global body styling */
+	main {
+		background-color: var(--color-background);
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
+	/* Message styles */
+	.message {
+		position: fixed;
+		width: 100%;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	.message button {
+		font-family: sans-serif;
+		cursor: pointer !important;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: inherit;
+		color: var(--color-text-primary);
+		background-color: transparent;
+		border: none;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,29 @@
-<h1>MS</h1>
-<p>...loading</p>
-<p>Visit <a href="https://meganspurney.github.io/">meganspurney.github.io</a> to see current work.</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		window.scrollTo(0, 0);
+	});
+</script>
+
+<main>
+	<h1>MS</h1>
+	<p>...loading</p>
+	<p>
+		Visit <a href="https://meganspurney.github.io/">meganspurney.github.io</a> to see current work.
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		position: relative;
+		width: 100%;
+		min-height: 100dvh;
+	}
+
+	a {
+		color: var(--color-secondary);
+		text-decoration: none;
+	}
+</style>
